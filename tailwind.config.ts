462a75095e607add10fbe827c95c321930d2d953
Garik <garik.sandrosyan@gmail.com>
2025-01-24 20:19:39 +0100

    Add Tailwind CSS configuration and types
    Introduce a `tailwind.config.ts` file with custom themes and colors to integrate Tailwind CSS into the project. Additionally, add `@types/tailwindcss` to the dependencies to enable TypeScript support for Tailwind.

---
+++ tailwind.config.ts
@@ -0,0 +1,29 @@
+export default {
+    content: ['./src/**/*.{html,js,jsx,ts,tsx}'], // Your project's file paths
+    theme: {
+        extend: {
+            colors: {
+                neutral950: '#0E121B',
+                neutral900: '#191B25',
+                neutral800: '#232530',
+                neutral700: '#2B303B',
+                neutral600: '#525866',
+                neutral500: '#717784',
+                neutral400: '#99A0AE',
+                neutral300: '#CACFD8',
+                neutral200: '#E0E4EA',
+                neutral100: '#F3F5F8',
+                neutral50: '#F5F7FA',
+                neutral0: '#FFFFFF',
+                blue700: '#2547D0',
+                blue500: '#335CFF',
+                blue50: '#EBF1FF',
+                green500: '#21C16B',
+                green100: '#D1FBE9',
+                red500: '#FB3748',
+                red100: '#FFD5D8',
+            },
+        },
+    },
+    plugins: [], // Add Tailwind plugins here if needed
+}
